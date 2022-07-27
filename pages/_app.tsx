59cabbcdb2ef491a80e6dbfbf5169276ce6d460e
Martin <vassmartin5@gmail.com>
2022-07-27 20:43:24 +0200

    fixing the bootstrap dropdown

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,8 @@ import { initializeApp } from "firebase/app";
 import { useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import useAppStore from '../store/useStore';
+import Head from 'next/head';
+import Script from 'next/script';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,7 +29,7 @@ export const app = initializeApp(firebaseConfig);
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
+
   const auth = getAuth();
   const setuser = useAppStore((state:any) => state.setuser)
   
@@ -44,7 +46,24 @@ function MyApp({ Component, pageProps }: AppProps) {
     console.log(app)
   }, [])
   
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+    // Responsive meta tag
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    //  bootstrap CDN
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+    crossOrigin="anonymous" 
+    />
+    </Head>
+      
+    <Script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+    crossOrigin="anonymous"/>
+    <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
